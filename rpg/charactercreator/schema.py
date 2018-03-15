5a2ac32adea6444e2d3f3a9f5d055df28debfa48
Cassidy SN Avery <tacolim@gmail.com>
2018-03-15 15:22:08 -0400

    graphql

--- rpg/charactercreator/schema.py
+++ rpg/charactercreator/schema.py
@@ -1,8 +1,24 @@
 from graphene_django import DjangoObjectType
 import graphene
 from .models import Character as CharacterModel
+from armory.models import Item as ItemModel
+from armory.models import Weapon as WeaponModel
+
+class Item(DjangoObjectType):
+    class Meta:
+        model = ItemModel
+
+class Weapon(DjangoObjectType):
+    class Meta:
+        model = WeaponModel
 
 class Character(DjangoObjectType):
+    inventory = graphene.List(Item)
+
+    @graphene.resolve_only_args
+    def resolve_inventory(self):
+        return self.inventory.all()
+
     class Meta:
         model = CharacterModel
 
